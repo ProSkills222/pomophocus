fb29f1e151a002c604f748bacab262bcc06143c9
Hai Bui <haibui.cs@gmail.com>
2023-04-15 18:35:31 +0700

    add task button

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import classes from "./App.module.css";
 import Timer from "./components/Timer/Timer";
+import Task from "./components/Task/Task";
 import { useSelector } from "react-redux";
 import clsx from "clsx";
 
@@ -13,9 +14,9 @@ function App() {
       <div className={classes.content}>
         <Timer />
       </div>
-      {/* <div className={classes.content}>
-        <Timer />
-      </div> */}
+      <div className={classes.content}>
+        <Task />
+      </div>
     </div>
   );
 }
